eea4ff2d11cefb711fd9e485c129be4f9dd0dd45
Combatd <combatdstar@gmail.com>
2019-12-26 15:31:22 -0800

    Define initialize constructor for PolyTreeNode

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,22 @@
 class PolyTreeNode
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
 
+    # get parent instance variable
+    def parent
+        @parent
+    end
+
+    # get children instance variable
+    def children
+        @children
+    end
+
+    # get value stored at node
+    def value
+        @value
+    end
 end
